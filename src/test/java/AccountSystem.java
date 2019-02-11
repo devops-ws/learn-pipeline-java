3e8fe5cf4d361ccb9f80a1ee3692c9fab1dec60a
Zhao Xiaojie <LinuxSuRen@gmail.com>
2019-02-11 20:02:17 +0800

    Add sample codes

---
+++ src/test/java/AccountSystem.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class AccountSystem {
+    @Test
+    public void name() {
+        assertTrue(true);
+    }
+}
---
+++ src/test/java/UserSystem.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class UserSystem {
+    @Test
+    public void name() {
+        boolean result = false;
+        assertTrue(result);
+    }
+}
