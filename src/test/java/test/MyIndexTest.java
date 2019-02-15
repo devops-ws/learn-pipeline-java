62a054f542518520e99b563cbf879cd4f40b9441
liuyanw.liu <liuyanw.liu@qunar.com>
2019-02-15 14:35:48 +0800

    完成我的页面主接口未登录、推荐位、Icons接口以及红点接口测试

--- src/test/java/test/MyIndexTest.java
+++ src/test/java/test/MyIndexTest.java
@@ -12,11 +12,12 @@ import java.util.logging.Logger;
 import static org.junit.Assert.assertTrue;
 
 public class MyIndexTest {
+    private final Logger logger = Logger.getLogger("javasoft");
     /**
      * 测试代码
      */
     public static void main (String[] args){
-        String url ="https://pay.qunar.com/mobile/h5/personalcenter/newIndexImprove.htm";
+        String url =PropertiesUtil.getProperty("redpoint.url");
         String responseStr = HttpClientUtil.doPost(url);
         JSONObject obj = JSON.parseObject(responseStr);
         String status = obj.getString("status");
@@ -25,20 +26,64 @@ public class MyIndexTest {
     }
 
     /**
-     * 我的页面，newIndexProver接口测试-未登录
+     * 我的页面，newIndexProver（主接口）测试-未登录
      */
     @Test
-    public void newIndex(){
-        String url = PropertiesUtil.getProperty("newIndex.url");
+    public void newIndexImprove(){
+        String url = PropertiesUtil.getProperty("newIndexImprove.url");
         String responseStr = HttpClientUtil.doPost(url);
         JSONObject obj = JSON.parseObject(responseStr);
         String status = obj.getString("status");
-       /* System.out.println(responseStr);
-        System.out.println(status);*/
-        Logger log = Logger.getLogger("javasoft");
-        log.setLevel(Level.INFO);
-        log.info(responseStr);
-        log.info(status);
+        logger.setLevel(Level.INFO);
+        logger.info("newIndexImprove：" + responseStr);
+        logger.info("newIndexImprove.status：" + status);
         assertTrue("22032".equals(obj.getString("status")));
+    }
+
+    /**
+     * 我的页面,recommend（运营位接口）测试-未登录
+     */
+    @Test
+    public void recommend(){
+        String url = PropertiesUtil.getProperty("recommend.url");
+        String responseStr = HttpClientUtil.doPost(url);
+        JSONObject obj = JSON.parseObject(responseStr);
+        String status = obj.getString("status");
+        logger.setLevel(Level.INFO);
+        logger.info("recommend：" + responseStr);
+        logger.info("recommend.status：" + status);
+        assertTrue("0".equals(obj.getString("status")));
+    }
+
+    /**
+     * 我的页面,showIcons（Icons接口）测试-未登录
+     */
+    @Test
+    public void showIcons(){
+        String url = PropertiesUtil.getProperty("showIcons.url");
+        String responseStr = HttpClientUtil.doPost(url);
+        JSONObject obj = JSON.parseObject(responseStr);
+        String status = obj.getString("status");
+        logger.setLevel(Level.INFO);
+        logger.info("showIcons：" + responseStr);
+        logger.info("showIcons.stauts：" + status);
+        assertTrue("0".equals(obj.getString("status")));
+    }
+
+
+
+    /**
+     * 我的页面,redpoint（红点接口）测试-未登录
+     */
+    @Test
+    public void redpoint(){
+        String url = PropertiesUtil.getProperty("redpoint.url");
+        String responseStr = HttpClientUtil.doPost(url);
+        JSONObject obj = JSON.parseObject(responseStr);
+        String status = obj.getString("status");
+        logger.setLevel(Level.INFO);
+        logger.info("redpoint：" + responseStr);
+        logger.info("redpoint.status："+ status);
+        assertTrue("0".equals(obj.getString("status")));
     }
 }
