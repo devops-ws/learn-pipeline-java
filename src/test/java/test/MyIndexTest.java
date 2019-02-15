341bae017a8c7f5ba3a316f023f9920dff23a1ad
liuyanw.liu <liuyanw.liu@qunar.com>
2019-02-15 10:48:21 +0800

    新增newIndex接口测试case

---
+++ src/test/java/test/MyIndexTest.java
@@ -0,0 +1,34 @@
+package test;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import org.junit.Test;
+import util.HttpClientUtil;
+
+import static org.junit.Assert.assertTrue;
+
+public class MyIndexTest {
+    /**
+     * 测试代码
+     */
+    public static void main (String[] args){
+        String url ="https://pay.qunar.com/mobile/h5/personalcenter/newIndexImprove.htm";
+        String responseStr = HttpClientUtil.doPost(url);
+        JSONObject obj = JSON.parseObject(responseStr);
+        String status = obj.getString("status");
+        System.out.println(responseStr);
+        System.out.println(status);
+    }
+
+    /**
+     * 我的页面，newIndexProver接口测试-未登录
+     */
+    @Test
+    public void newIndex(){
+        String url ="https://pay.qunar.com/mobile/h5/personalcenter/newIndexImprove.htm";
+        String responseStr = HttpClientUtil.doPost(url);
+        JSONObject obj = JSON.parseObject(responseStr);
+        //String status = obj.getString("status");
+        assertTrue("22032".equals(obj.getString("status")));
+    }
+}
