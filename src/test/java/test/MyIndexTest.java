7b19096e45d1e4b3f6bca8458f87b4940e645dbc
liuyanw.liu <liuyanw.liu@qunar.com>
2019-02-18 16:45:30 +0800

    完成showIconsrn接口测试case

--- src/test/java/test/MyIndexTest.java
+++ src/test/java/test/MyIndexTest.java
@@ -6,6 +6,8 @@ import org.junit.Test;
 import util.HttpClientUtil;
 import util.PropertiesUtil;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -17,11 +19,19 @@ public class MyIndexTest {
      * 测试代码
      */
     public static void main (String[] args){
-        String url =PropertiesUtil.getProperty("redpoint.url");
-        String responseStr = HttpClientUtil.doPost(url);
+        String url =PropertiesUtil.getProperty("showIconsrn.url");
+        Map<String,String> param = new HashMap<String,String>();
+        param.put("scheme","qunaraphone");
+        param.put("isRN","true");
+        param.put("qpVersion","32");
+        Map<String,String> hearder = new HashMap<String,String>();
+        hearder.put("Cookie","QN1=000015000600105c80c0fe20; QN48=tc_3149f483c5f4e96c_168ac300f4d_4784; QN300=organic; QN235=2019-02-18; _i=ue-xY4nyAVjXXXhy46BjDAKxT4yX; _vi=hNwn1H4wgZCT2xfQFk7XaJPgFNj2hU8sofoCjpYMQVE6H8ny_fF3O6O365rmcrPLmMaDfFuQrTmnAEm8soAdM2TKrvRdsWWYqJoZZlqhGfhzzrdNAQZG2cwivz7ahoOvogPxVNdyN7jEDAPtcvBxZ2q75OEtsH64Smt462DO9vZZ; fid=20132569-ea87-46b8-a3f1-eca497d4010b; QN241=; QN270=1111%2C60001246%2CC5065%2CAA3A1BAC-8FE5-D291-7A30-EA36647C4D48%2C10010; csrfToken=ULW1fDPwz3pSJDK; _v=GfCYmRk0kYhpRxMB3ZoDVKxVj_dnUDyJmlWzLRhPk2soqmnRl1eQMpxqqHadx8avmeJscPx0QasxuMX2uESWc9kYHIlyf0npzRXIwIAQEaFlyRwhCvtzYPjXwNAfdCrycI-z1z0GxLiPR3k5cpv8b_6UYKR9J_DmIIpEyfIbmKLH; _t=26080242; _q=U.gscsldt7224; _s=s_HJHX5SDL6UJ6WUIEPDYAOSCV4Q");
+        //mobile/h5/personalcenter/showIconsrn.htm?scheme=qunaraphone&isRN=true&qpVersion=32
+        String responseStr = HttpClientUtil.doGet(url,param,hearder);
+                //HttpClientUtil.doPost(url);
         JSONObject obj = JSON.parseObject(responseStr);
         String status = obj.getString("status");
-        System.out.println(responseStr);
+        System.out.println("responseStr:" + responseStr);
         System.out.println(status);
     }
 
@@ -86,4 +96,28 @@ public class MyIndexTest {
         logger.info("redpoint.status："+ status);
         assertTrue("0".equals(obj.getString("status")));
     }
+
+
+    /**
+     * 我的页面,showIconsrn（Iconsrn接口）测试-已登录
+     */
+    @Test
+    public void showIconsrn(){
+        String url = PropertiesUtil.getProperty("showIconsrn.url");
+        Map<String,String> param = new HashMap<String,String>();
+        param.put("scheme","qunaraphone");
+        param.put("isRN","true");
+        param.put("qpVersion","32");
+        Map<String,String> hearder = new HashMap<String,String>();
+        hearder.put("Cookie","QN1=000015000600105c80c0fe20; QN48=tc_3149f483c5f4e96c_168ac300f4d_4784; QN300=organic; QN235=2019-02-18; _i=ue-xY4nyAVjXXXhy46BjDAKxT4yX; _vi=hNwn1H4wgZCT2xfQFk7XaJPgFNj2hU8sofoCjpYMQVE6H8ny_fF3O6O365rmcrPLmMaDfFuQrTmnAEm8soAdM2TKrvRdsWWYqJoZZlqhGfhzzrdNAQZG2cwivz7ahoOvogPxVNdyN7jEDAPtcvBxZ2q75OEtsH64Smt462DO9vZZ; fid=20132569-ea87-46b8-a3f1-eca497d4010b; QN241=; QN270=1111%2C60001246%2CC5065%2CAA3A1BAC-8FE5-D291-7A30-EA36647C4D48%2C10010; csrfToken=ULW1fDPwz3pSJDK; _v=GfCYmRk0kYhpRxMB3ZoDVKxVj_dnUDyJmlWzLRhPk2soqmnRl1eQMpxqqHadx8avmeJscPx0QasxuMX2uESWc9kYHIlyf0npzRXIwIAQEaFlyRwhCvtzYPjXwNAfdCrycI-z1z0GxLiPR3k5cpv8b_6UYKR9J_DmIIpEyfIbmKLH; _t=26080242; _q=U.gscsldt7224; _s=s_HJHX5SDL6UJ6WUIEPDYAOSCV4Q");
+        //mobile/h5/personalcenter/showIconsrn.htm?scheme=qunaraphone&isRN=true&qpVersion=32
+        String responseStr = HttpClientUtil.doGet(url,param,hearder);
+        JSONObject obj = JSON.parseObject(responseStr);
+        String status = obj.getString("status");
+        logger.setLevel(Level.INFO);
+        logger.info("showIconsrn：" + responseStr);
+        logger.info("showIconsrn.stauts：" + status);
+        assertTrue("0".equals(obj.getString("status")));
+    }
+
 }
--- src/test/java/util/HttpClientUtil.java
+++ src/test/java/util/HttpClientUtil.java
@@ -1,6 +1,9 @@
 package util;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -12,6 +15,7 @@ import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
@@ -24,7 +28,7 @@ public class HttpClientUtil {
      * @param param 请求参数
      * @return 结果
      */
-    public static String doGet(String url, Map<String, String> param) {
+    public static String doGet(String url, Map<String, String> param,Map<String, String> headers) {
 
         // 创建Httpclient对象
         CloseableHttpClient httpclient = HttpClients.createDefault();
@@ -39,10 +43,18 @@ public class HttpClientUtil {
                     builder.addParameter(key, param.get(key));
                 }
             }
+
             URI uri = builder.build();
+            String urlStr = uri.toString();
+            System.out.println("urlStr:" + urlStr);
 
             // 创建http GET请求
             HttpGet httpGet = new HttpGet(uri);
+            if(headers!=null){
+                for(Map.Entry<String,String> en:headers.entrySet()){
+                    httpGet.setHeader(en.getKey(), en.getValue());
+                }
+            }
 
             // 执行请求
             response = httpclient.execute(httpGet);
@@ -65,13 +77,21 @@ public class HttpClientUtil {
         return resultString;
     }
 
+
+
+
+
+
+
+
+
     /**
      * 无参数的get请求
      * @param url 请求地址
      * @return 返回结果
      */
     public static String doGet(String url) {
-        return doGet(url, null);
+        return doGet(url, null,null);
     }
 
     /**
@@ -115,6 +135,49 @@ public class HttpClientUtil {
         return resultString;
     }
 
+
+    public static String doPost(String url,Map<String,String> params,Map<String,String> headers) {
+        CloseableHttpClient client = HttpClients.createDefault();
+        HttpPost httpPost = new HttpPost(url);
+        UrlEncodedFormEntity entity;
+        List<NameValuePair> paramPairs = new ArrayList<NameValuePair>();
+        if(params!=null){
+            for(Map.Entry<String,String> en:params.entrySet()){
+                paramPairs.add(new BasicNameValuePair(en.getKey(), en.getValue()));
+            }
+        }
+        if(headers!=null){
+            for(Map.Entry<String,String> en:headers.entrySet()){
+                httpPost.setHeader(en.getKey(), en.getValue());
+            }
+        }
+        try {
+            entity = new UrlEncodedFormEntity(paramPairs, "UTF-8");
+            httpPost.setEntity(entity);
+            HttpResponse resp = client.execute(httpPost);
+            HttpEntity respEntity = resp.getEntity();
+            if (null != respEntity) {
+                return EntityUtils.toString(respEntity, "UTF-8");
+            }
+
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                client.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+
+
     /**
      * 无参的post请求
      * @param url 请求url
@@ -123,4 +186,6 @@ public class HttpClientUtil {
     public static String doPost(String url) {
         return doPost(url, null);
     }
+
+
 }
