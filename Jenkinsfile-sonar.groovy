5a48ea9c8289563f1c76729b4f67956234482544
Zhao Xiaojie <rick@jenkins-zh.cn>
2019-10-14 16:28:19 +0800

    Create Jenkinsfile-sonar.groovy

---
+++ Jenkinsfile-sonar.groovy
@@ -0,0 +1,27 @@
+pipeline {
+  agent {
+    label 'maven'
+  }
+  
+  stages{
+    stage('build'){
+      steps{
+        container('java'){
+          sh 'mvn clean package'
+        }
+      }
+    }
+
+    stage('sonar scan') {
+      steps {
+        sh '''
+          echo $sonar.host.url
+          mvn sonar:sonar \
+            -Dsonar.projectKey=df \
+            -Dsonar.host.url=$sonar.host.url \
+            -Dsonar.login=$sonar.login
+        '''
+      }
+    }
+  }
+}
